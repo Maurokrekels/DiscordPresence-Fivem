ca9308c529fbdab2bfb11e9e80d60fb466ec9e90
Mauro <99716153+Maurokrekels@users.noreply.github.com>
2023-10-10 23:56:17 +0200

    Fivem Discord Presence

---
+++ client.lua
@@ -0,0 +1,20 @@
+local appId = 112233445566778899 -- Jou app id hier
+local RichPresence = "" -- jou tekst hier
+
+local tekst = '' -- Jou tekst hier
+
+local DiscordLink = "discord.gg/" -- jou discord link hier
+local DiscordTekst = "Join onze Discord!" -- jou discord tekst hier
+
+local ConnectTekst = "Join onze Server!" -- Jou connect tekst hier
+local ConnectLink = "https://cfx.re/join/" -- Jou connect link hier
+
+
+Citizen.CreateThread(function()
+    SetRichPresence(RichPresence)
+	SetDiscordAppId(appId)
+	SetDiscordRichPresenceAsset('logo')
+	SetDiscordRichPresenceAssetSmallText(tekst)
+	SetDiscordRichPresenceAction(0, ConnectTekst, ConnectLink)
+	SetDiscordRichPresenceAction(1, DiscordTekst, DiscordLink)
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'MauroKrekels'
+description 'Discord Presence'
+version '1.0'
+
+client_scripts {
+	'client.lua'
+}
